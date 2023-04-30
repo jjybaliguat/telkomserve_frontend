81f207a4fefdb7d0246ee846db88badb1e98fb14
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-04-30 18:35:03 +0800

    initial deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     nodeEnv: 'production',
     DEV_APP_API: 'http://localhost:5000/api/v1',
     // PRODUCTION_APP_API: 'https://rdnaks-api.onrender.com/api/v1',
-    PRODUCTION_APP_API: 'http://212.1.213.208/api/v1',
+    PRODUCTION_APP_API: 'https://api.rdnaksnds.com/api/v1',
     DEV_APP_URL: 'http://localhost:3000',
     APP_URL: 'https://rdnaksict.vercel.app',
     GOOGLE_API: 'AIzaSyC6LsjcErgaKvmmIJcWfIJyBkf3SxFivNc',
