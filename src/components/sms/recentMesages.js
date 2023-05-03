8098205edf99110eb79a5d17c823a6ba590e93eb
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-05-03 08:16:39 +0800

    some changes

--- src/components/sms/recentMesages.js
+++ src/components/sms/recentMesages.js
@@ -9,7 +9,7 @@ import Empty from '../svgIcons/Empty';
 import dayjs from 'dayjs'
 
 Array.prototype.sortBy = function(p) {
-    return this.slice(0).sort(function(b,a) {
+    return this.slice(0).sort(function(a,b) {
       return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
     });
 }
