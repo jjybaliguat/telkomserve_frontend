d13b51910016e3030a2cc809b8b7befa9775e322
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-05-17 16:16:44 +0800

    some changes

---
+++ src/components/chatbot/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { useRef } from "react";
+
+const ChatBot = () => {
+    const MessengerRef = useRef();
+    useEffect(() => {
+        MessengerRef.current.setAttribute('page_id', 'justinwifivendo');
+        MessengerRef.current.setAttribute('attribution', 'biz_inbox');
+    
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                xfbml: true,
+                version: 'v16.0',
+            });
+        };
+        (function (d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }, []);
+    return (
+        <>
+            <div id="fb-root"></div>
+            <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div>
+        </>
+    );
+    }; 
+    export default ChatBot;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Landing from '../components/landingpage'
+import ChatBot from '../components/chatbot'
 
 const index = () => {
   return (
     <>
     <Landing />
+    <ChatBot />
     </>
   )
 }
